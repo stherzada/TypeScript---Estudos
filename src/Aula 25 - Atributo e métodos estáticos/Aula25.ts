d09f5681c5876364340e805f79d5b27540a3bf0c
Sthefany Sther <sthefanypequeno@gmail.com>
2023-09-12 10:52:29 -0300

    fix: syntax change

---
+++ src/Aula 25 - Atributo e métodos estáticos/Aula25.ts
@@ -0,0 +1,33 @@
+export class Person {
+    static agePattern = 0
+    static cpfPattern = '000.000.000-00'
+    public name: string;
+    public lastname: string;
+    public _age: number;
+    private _cpf: string;
+    constructor(name: string, lastname: string, age: number, cpf: string) {
+        this.name = name;
+        this.lastname = lastname;
+        this._age = age;
+        this._cpf = cpf;
+    }
+
+    //chamando o método pela instancia
+    methodNormal(): void{
+        console.log(Person.agePattern, Person.cpfPattern)
+    }
+
+  static createPerson(name: string, lastname: string): Person{
+    return new Person(name, lastname, Person.agePattern, Person.cpfPattern )
+  }
+}
+
+
+const person1 = new Person("Sther", "Zada", 60, "560.000.000-00");
+const person2 = Person.createPerson("Lucas", "Souza");
+console.log(person1)
+console.log(person2)
+
+
+person1.methodNormal()
+console.log(Person.agePattern, Person.cpfPattern)
---
+++ src/Aula 26 - Construtor privado e singleton/Aula26.ts
@@ -0,0 +1,39 @@
+//singleton  - GoF | Factory Method - GoF
+
+export class DataBase{
+    private static database: DataBase;
+    private host: string;
+    private user: string;
+    private password: string;
+
+   private constructor(host: string,  user:string, password: string){
+        this.host = host;
+        this.user = user;
+        this.password = password;
+
+    }
+    
+    connect(): void{
+        console.log(`Connect: ${this.host} ${this.user} ${this.password}`)
+    }
+
+
+    //criado nossa instância
+    static getDatabase(host: string,  user:string, password: string): DataBase{
+
+        if(DataBase.database) return DataBase.database
+
+        DataBase.database = new DataBase(host, user, password)
+        return DataBase.database
+    }
+}
+
+const database1 = DataBase.getDatabase('localhost', 'rest', '123342')
+
+database1.connect()
+
+
+// const database2 = ('localhost', 'rest', '123342')
+
+// database2.connect()
+
