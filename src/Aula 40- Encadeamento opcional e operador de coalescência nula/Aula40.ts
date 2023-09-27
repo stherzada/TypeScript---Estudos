03c597710f08dafdecfe56fe197d3fa90cea9cd5
Sthefany Sther <sthefanypequeno@gmail.com>
2023-09-27 12:49:08 -0300

    test:test

---
+++ src/Aula 40- Encadeamento opcional e operador de coalescência nula/Aula40.ts
@@ -0,0 +1,19 @@
+type Archive = {
+    title: string;
+    text: string;
+    data?: Date;
+};
+
+const archive: Archive = {
+    title: "The title",
+    text: " The text",
+    // data: new Date(),
+};
+
+// o operador de coalescenência só verificar isso quando por null ou undefined
+console.log(archive.data?.toDateString() ?? "EITAH, don't have a date");
+console.log(undefined ?? "2-EITAH, don't have a date");
+console.log(null ?? "3-EITAH, don't have a date");
+console.log(false ?? "EITAH, don't have a date");
+console.log(0 ?? "EITAH, don't have a date");
+console.log("_" ?? "EITAH, don't have a date");
