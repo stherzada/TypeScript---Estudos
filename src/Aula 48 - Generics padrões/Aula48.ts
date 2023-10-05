7ec6108c7fb75fbbefe150b72cbd7c1168340d8a
Sthefany Sther <sthefanypequeno@gmail.com>
2023-10-05 15:22:33 -0300

    feat: exercises and generics types

--- src/Aula 48 - Generics padrões/Aula48.ts
+++ src/Aula 48 - Generics padrões/Aula48.ts
@@ -32,11 +32,45 @@ console.log(object2);
 //EXTRACT AND EXCLUDE
 type ABC = "A" | "B" | "C";
 type CDE = "C" | "D" | "E";
+
 //computa todos os tipos e exclui o repetido
 type TypeExclude = Exclude<ABC, CDE>;
 
 //computa e só mostra o que repete nos dois
 type TypeExtract = Extract<ABC, CDE>;
 
+//
+type AccountMDB = {
+    _id: string;
+    name: string;
+    age: number;
+    lastname: string;
+};
+
+type AccountApi = {
+    id: string;
+    name: string;
+    age: number;
+    lastname: string;
+};
+
+// type AccountApi = Pick<AccountMDB, Exclude<keyof AccountMDB, "_id">> & {
+//     id: string;
+// };
+
+const accountMDB: AccountMDB = {
+    _id: "ab34-shdhuw00223",
+    name: "Sther",
+    age: 34,
+    lastname: "sther",
+};
+
+function mapAccount(accountMDB: AccountMDB): AccountApi {
+    const { _id, ...accountData } = accountMDB;
+    return { ...accountData, id: _id };
+}
+
+const AccountApi = mapAccount(accountMDB);
+console.log(AccountApi);
 //
 export default 1;
---
+++ src/Aula 49 - Exercicio/Aula49.ts
@@ -0,0 +1,45 @@
+type VotationOption = {
+    numberOfvotes: number;
+    option: string;
+};
+
+export class Votation {
+    private _votationOptions: VotationOption[] = [];
+    constructor(public details: string) {}
+
+    addVotationOptions(votationOptions: VotationOption): void {
+        this._votationOptions.push(votationOptions);
+    }
+    vote(votationIndex: number): void {
+        if (!this._votationOptions[votationIndex]) return;
+        this._votationOptions[votationIndex].numberOfvotes += 1;
+    }
+    get votationOptions(): VotationOption[] {
+        return this._votationOptions;
+    }
+}
+
+export class VotationApp {
+    private votations: Votation[] = [];
+    addVotation(votation: Votation): void {
+        this.votations.push(votation);
+    }
+    showVotations(): void {
+        for (const votation of this.votations) {
+            console.log(votation.details);
+            for (const votationOption of votation.votationOptions) {
+                console.log(votationOption.option);
+            }
+        }
+    }
+}
+
+const votationUno = new Votation("What is your favorite programming language?");
+votationUno.addVotationOptions({ option: "TypeScript", numberOfvotes: 0 });
+votationUno.addVotationOptions({ option: "JavaScript", numberOfvotes: 0 });
+votationUno.addVotationOptions({ option: "Python", numberOfvotes: 0 });
+
+const votationApp = new VotationApp();
+votationApp.addVotation(votationUno);
+
+votationApp.showVotations();
